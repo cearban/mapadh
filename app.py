2abd304a48fd81e2175357b3505e266e259b1997
James Crone <james.crone@ed.ac.uk>
2024-08-27 15:10:27 +0100

    take Pg connection string from a config file

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ james@yggdrasil:~/PycharmProjects/mapadh$ export FLASK_APP=app.py
 james@yggdrasil:~/PycharmProjects/mapadh$ flask run
 """
 
+import configparser
+import os
 from flask import Flask
 from flask import render_template
 from flask import jsonify
 from .censustrips import fetch_population_along_route
 app = Flask(__name__)
+
+if not os.path.exists('CONFIG.ini'):
+    print("Warning - no CONFIG.ini with the Pg connection string found")
+
+# TODO: do config better: https://flask.palletsprojects.com/en/3.0.x/tutorial/factory/#the-application-factory
+config = configparser.ConfigParser()
+config.read('CONFIG.ini')
+PG_CONNECTION_STRING = config['Connections']['mapadh-db']
 
 
 @app.route('/')
@@ -22,7 +32,7 @@ def censustrips_route(route_id):
     :param route_id: 1
     :return: rendered html template
     """
-    route_stops_d = fetch_population_along_route(route_id)
+    route_stops_d = fetch_population_along_route(route_id, pg_conn_str=PG_CONNECTION_STRING)
 
     # flask knows to look in the templates folder
     # as we have designated our project as a package
@@ -33,13 +43,12 @@ def censustrips_route(route_id):
 @app.route('/censustrips_route_json/<int:route_id>')
 def censustrips_route_json(route_id):
     """
-    http://127.0.0.1:5000/censustrips_route_json/1 Glasgow -> Edinburgh
     http://127.0.0.1:5000/censustrips_route_json/2 Ayr -> Glasgow
     http://127.0.0.1:5000/censustrips_route_json/3 Edinburgh -> Kings Cross
     :param route_id: 1
     :return: dict as a json
     """
-    route_stops_d = fetch_population_along_route(route_id)
+    route_stops_d = fetch_population_along_route(route_id, pg_conn_str=PG_CONNECTION_STRING)
     return jsonify(route_stops_d)
 
 
