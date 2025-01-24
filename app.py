da589a42d67302bfd6558742dc7ba3fb24ec8efd
cearban <james.rc.crone@gmail.com>
2025-01-24 22:11:14 +0000

    added more useful comments

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ if not os.path.exists('CONFIG.ini'):
 # TODO: do config better: https://flask.palletsprojects.com/en/3.0.x/tutorial/factory/#the-application-factory
 config = configparser.ConfigParser()
 config.read('CONFIG.ini')
+# postgresql+psycopg2://username:password@host:port/dbname
 PG_CONNECTION_STRING = config['Connections']['mapadh-db']
 
 
@@ -28,7 +29,7 @@ def index():
 @app.route('/censustrips_route/<int:route_id>')
 def censustrips_route(route_id):
     """
-    http://127.0.0.1:5000/censustrips_route/1
+    http://127.0.0.1:5000/censustrips_route/3
     :param route_id: 1
     :return: rendered html template
     """
@@ -43,7 +44,6 @@ def censustrips_route(route_id):
 @app.route('/censustrips_route_json/<int:route_id>')
 def censustrips_route_json(route_id):
     """
-    http://127.0.0.1:5000/censustrips_route_json/2 Ayr -> Glasgow
     http://127.0.0.1:5000/censustrips_route_json/3 Edinburgh -> Kings Cross
     :param route_id: 1
     :return: dict as a json
