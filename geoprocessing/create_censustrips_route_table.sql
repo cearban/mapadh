24071603b3986023f756eb70d04d719f6b4d0dc4
cearban <james.rc.crone@gmail.com>
2025-01-24 21:39:02 +0000

    sql to create table

---
+++ geoprocessing/create_censustrips_route_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE censustrips.censustrips_route(
+    id serial primary key,
+    route_id integer,
+    seq_id integer,
+    station_id integer references censustrips.railway_stations  (gid)
+)
