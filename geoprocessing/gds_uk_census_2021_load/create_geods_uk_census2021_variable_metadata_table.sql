edc5741d16aa52f0f0cdf201874d9898dcfc43d2
James Crone <james.crone@ed.ac.uk>
2026-04-03 15:34:16 +0100

    sql to create GeoDS metadata table

---
+++ geoprocessing/gds_uk_census_2021_load/create_geods_uk_census2021_variable_metadata_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE metadata.geods_uk_census2021_variable_metadata (
+	id serial4 NOT NULL,
+	variable_id text NULL,
+	variable_name text NULL,
+	table_id text NULL,
+	table_name text NULL,
+	unit_of_measure text NULL
+);
